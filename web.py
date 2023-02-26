f2126033aeb058469827549be53da3ab3f351c40
Evmorfia Elpida Dasyra <dasyevita@gmail.com>
2023-02-26 19:16:04 +0200

    change design

--- web.py
+++ web.py
@@ -2,6 +2,8 @@ import streamlit as st
 import functions as f
 todos=f.getTodos()
 
+st.set_page_config(layout="wide")
+
 def addTodo():
     todo = st.session_state["newTodo"] + "\n"
     todos.append(todo)
@@ -9,7 +11,8 @@ def addTodo():
 
 st.title("My Todo App")
 st.subheader("This is my first web app.")
-st.write("This app helps to increase your productivity. Go ahead and try it!")
+st.write("This app helps to increase your <b>productivity</b>. Go ahead and try it!<br><br>", unsafe_allow_html=True)
+
 
 for index,todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -19,7 +22,9 @@ for index,todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
+st.write("<br>", unsafe_allow_html=True)
+
+
 st.text_input(label="Enter a todo: ", placeholder="Write here the new todo..",
               on_change=addTodo, key="newTodo")
-
 # st.session_state
